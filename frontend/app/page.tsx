122696062e8806d7107086dfed97dcb5a6f196dc
KalapalaGIT <lassi.uk@gmail.com>
2025-04-01 08:27:08 +0300

    Styles work

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,11 +3,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Link href="/notes">Notes</Link>
-      <br />
-      <Link href="/studytools">Studytools</Link>
+    <div className="flex justify-between">
+      <div>
+        <h1>Hello World</h1>
+        <Link href="/notes">Notes</Link>
+        <br />
+        <Link href="/studytools">Studytools</Link>
+      </div>
+      <div>
+        <h1>Middle</h1>
+      </div>
+      <div>
+        <h1>Right</h1>
+      </div>
     </div>
   );
 }
